d875231260d0fd6e8f4f6270dee52f591bb1ac09
peakchen90 <1558267774@qq.com>
2023-05-04 00:38:21 +0800

    chore: update

---
+++ src/main/java/com/gumingnc/mars_support/completion/AppJsonCompletionContributor.java
@@ -0,0 +1,27 @@
+package com.gumingnc.mars_support.completion;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+public class AppJsonCompletionContributor extends CompletionContributor {
+    public AppJsonCompletionContributor() {
+        extend(
+                CompletionType.BASIC,
+                PlatformPatterns.psiElement(),
+                new CompletionProvider<CompletionParameters>() {
+                    @Override
+                    protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
+                        result.getPrefixMatcher();
+                    }
+
+                    //                    public void addCompletions(@NotNull CompletionParameters parameters,
+                    //                                               @NotNull ProcessingContext context,
+                    //                                               @NotNull CompletionResultSet resultSet) {
+                    //                        resultSet.addElement(LookupElementBuilder.create("Hello"));
+                    //                    }
+                }
+        );
+    }
+}
